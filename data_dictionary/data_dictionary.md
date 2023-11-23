c2248566cbf9ae9d6b14a2d586b46533956ca3bd
Rashid-Afkhami <rash.afkhami@gmail.com>
2023-11-23 12:46:47 +0800

    data dict updated

---
+++ data_dictionary/data_dictionary.md
@@ -0,0 +1,77 @@
+# Data Dictionary
+
+## fact_immigration
+| Column | Description | Data Type | Example |
+|--------|-------------|-----------|---------|
+| uid | Unique identifier for each immigration record | Integer | 12345 |
+| year | 4-digit year of the immigration record | Integer | 2019 |
+| month | Numeric month of the immigration record | Integer | 7 |
+| city_code | Port of entry for immigration | String | NYC |
+| state_code | State of arrival for immigration | String | NY |
+| arrive_date | Arrival date in SAS format | Date | 2019-07-15 |
+| departure_date | Departure date in SAS format | Date | 2019-07-25 |
+| mode | Mode of transportation | Integer | 1 |
+| visa | Visa type | Integer | 2 |
+| country | Country | String | United States |
+
+## dim_immigration_personal
+| Column | Description | Data Type | Example |
+|--------|-------------|-----------|---------|
+| uid | Unique identifier for each immigration personal record | Integer | 67890 |
+| citizen_country | Citizen country code | Integer | 123 |
+| residence_country | Residence country code | Integer | 456 |
+| birth_year | Year of birth | Integer | 1985 |
+| gender | Gender of the immigrant | String | Male |
+| ins_num | INS number | String | 123456789 |
+
+## dim_immigration_airline
+| Column | Description | Data Type | Example |
+|--------|-------------|-----------|---------|
+| uid | Unique identifier for each immigration airline record | Integer | 54321 |
+| airline | Airline code | String | AA |
+| admission_num | Admission number | String | 987654321 |
+| flight_number | Flight number | String | AA123 |
+| visa_type | Visa type | String | B1 |
+
+## dim_temperature
+| Column | Description | Data Type | Example |
+|--------|-------------|-----------|---------|
+| date | Date of temperature measurement | Date | 2019-07-15 |
+| avg_temperature | Average temperature | Double | 78.5 |
+| avg_temperature_uncertainty | Uncertainty in average temperature | Double | 0.5 |
+| city | City where temperature was measured | String | New York |
+| country | Country where temperature was measured | String | United States |
+
+## dim_demography
+| Column | Description | Data Type | Example |
+|--------|-------------|-----------|---------|
+| city | City name | String | New York |
+| state | State name | String | New York |
+| male_population | Male population count | Integer | 50000 |
+| female_population | Female population count | Integer | 55000 |
+| num_veterans | Number of veterans | Integer | 7500 |
+| foreign_born | Count of foreign-born residents | Integer | 2000 |
+| race | Race | String | White |
+
+## dim_demography_stats
+| Column | Description | Data Type | Example |
+|--------|-------------|-----------|---------|
+| city | City name | String | New York |
+| state | State name | String | New York |
+| median_age | Median age of residents | Double | 35.5 |
+| avg_household_size | Average household size | Double | 2.5 |
+
+## dim_airport
+| Column | Description | Data Type | Example |
+|--------|-------------|-----------|---------|
+| ident | Airport identifier | String | JFK |
+| type | Airport type | String | Large |
+| airport_name | Airport name | String | John F. Kennedy International Airport |
+| elevation_ft | Elevation in feet | Integer | 10 |
+| iso_country | ISO country code | String | US |
+| iso_region | ISO region code | String | US-NY |
+| municipality | Municipality | String | New York |
+| gps_code | GPS code | String | JFK |
+| local_code | Local code | String | JFK |
+| coordinates | Coordinates | String | 40.6413° N, 73.7781° W |
+
--- data_dictionary/print_data_dictionary.py
+++ data_dictionary/print_data_dictionary.py
@@ -1,105 +1,89 @@
 table_columns = {
     'fact_immigration': {
-        'Old Column Names': ['cicid', 'i94yr', 'i94mon', 'i94port', 'i94addr', 'arrdate', 'depdate', 'i94mode', 'i94visa', 'country'],
-        'New Column Names': ['uid', 'year', 'month', 'city_code', 'state_code', 'arrive_date', 'departure_date', 'mode', 'visa', 'country'],
-        'Description': [
-            'Unique identifier for each immigration record',
-            '4-digit year of the immigration record',
-            'Numeric month of the immigration record',
-            'Port of entry for immigration',
-            'State of arrival for immigration',
-            'Arrival date in SAS format',
-            'Departure date in SAS format',
-            'Mode of transportation',
-            'Visa type',
-            'Country',
+        'Columns': [
+            ('uid', 'Unique identifier for each immigration record', 'Integer', '12345'),
+            ('year', '4-digit year of the immigration record', 'Integer', '2019'),
+            ('month', 'Numeric month of the immigration record', 'Integer', '7'),
+            ('city_code', 'Port of entry for immigration', 'String', 'NYC'),
+            ('state_code', 'State of arrival for immigration', 'String', 'NY'),
+            ('arrive_date', 'Arrival date in SAS format', 'Date', '2019-07-15'),
+            ('departure_date', 'Departure date in SAS format', 'Date', '2019-07-25'),
+            ('mode', 'Mode of transportation', 'Integer', '1'),
+            ('visa', 'Visa type', 'Integer', '2'),
+            ('country', 'Country', 'String', 'United States')
         ]
     },
     'dim_immigration_personal': {
-        'Old Column Names': ['cicid', 'i94cit', 'i94res', 'biryear', 'gender', 'insnum'],
-        'New Column Names': ['uid', 'citizen_country', 'residence_country', 'birth_year', 'gender', 'ins_num'],
-        'Description': [
-            'Unique identifier for each immigration personal record',
-            'Citizen country code',
-            'Residence country code',
-            'Year of birth',
-            'Gender of the immigrant',
-            'INS number'
+        'Columns': [
+            ('uid', 'Unique identifier for each immigration personal record', 'Integer', '67890'),
+            ('citizen_country', 'Citizen country code', 'Integer', '123'),
+            ('residence_country', 'Residence country code', 'Integer', '456'),
+            ('birth_year', 'Year of birth', 'Integer', '1985'),
+            ('gender', 'Gender of the immigrant', 'String', 'Male'),
+            ('ins_num', 'INS number', 'String', '123456789')
         ]
     },
     'dim_immigration_airline': {
-        'Old Column Names': ['cicid', 'airline', 'admnum', 'fltno', 'visatype'],
-        'New Column Names': ['uid', 'airline', 'admission_num', 'flight_number', 'visa_type'],
-        'Description': [
-            'Unique identifier for each immigration airline record',
-            'Airline code',
-            'Admission number',
-            'Flight number',
-            'Visa type'
+        'Columns': [
+            ('uid', 'Unique identifier for each immigration airline record', 'Integer', '54321'),
+            ('airline', 'Airline code', 'String', 'AA'),
+            ('admission_num', 'Admission number', 'String', '987654321'),
+            ('flight_number', 'Flight number', 'String', 'AA123'),
+            ('visa_type', 'Visa type', 'String', 'B1')
         ]
     },
     'dim_temperature': {
-        'Old Column Names': ['dt', 'AverageTemperature', 'AverageTemperatureUncertainty', 'City', 'Country'],
-        'New Column Names': ['date', 'avg_temperature', 'avg_temperature_uncertainty', 'city', 'country'],
-        'Description': [
-            'Date of temperature measurement',
-            'Average temperature',
-            'Uncertainty in average temperature',
-            'City where temperature was measured',
-            'Country where temperature was measured'
+        'Columns': [
+            ('date', 'Date of temperature measurement', 'Date', '2019-07-15'),
+            ('avg_temperature', 'Average temperature', 'Double', '78.5'),
+            ('avg_temperature_uncertainty', 'Uncertainty in average temperature', 'Double', '0.5'),
+            ('city', 'City where temperature was measured', 'String', 'New York'),
+            ('country', 'Country where temperature was measured', 'String', 'United States')
         ]
     },
     'dim_demography': {
-        'Old Column Names': ['City', 'State', 'Male Population', 'Female Population', 'Number of Veterans', 'Foreign-born', 'Race'],
-        'New Column Names': ['city', 'state', 'male_population', 'female_population', 'num_veterans', 'foreign_born', 'race'],
-        'Description': [
-            'City name',
-            'State name',
-            'Male population count',
-            'Female population count',
-            'Number of veterans',
-            'Count of foreign-born residents',
-            'Race'
+        'Columns': [
+            ('city', 'City name', 'String', 'New York'),
+            ('state', 'State name', 'String', 'New York'),
+            ('male_population', 'Male population count', 'Integer', '50000'),
+            ('female_population', 'Female population count', 'Integer', '55000'),
+            ('num_veterans', 'Number of veterans', 'Integer', '7500'),
+            ('foreign_born', 'Count of foreign-born residents', 'Integer', '2000'),
+            ('race', 'Race', 'String', 'White')
         ]
     },
     'dim_demography_stats': {
-        'Old Column Names': ['City', 'State', 'Median Age', 'Average Household Size'],
-        'New Column Names': ['city', 'state', 'median_age', 'avg_household_size'],
-        'Description': [
-            'City name',
-            'State name',
-            'Median age of residents',
-            'Average household size'
+        'Columns': [
+            ('city', 'City name', 'String', 'New York'),
+            ('state', 'State name', 'String', 'New York'),
+            ('median_age', 'Median age of residents', 'Double', '35.5'),
+            ('avg_household_size', 'Average household size', 'Double', '2.5')
         ]
     },
     'dim_airport': {
-        'Old Column Names': ['ident', 'type', 'name', 'elevation_ft', 'iso_country', 'iso_region', 'municipality', 'gps_code', 'local_code', 'coordinates'],
-        'New Column Names': ['ident', 'type', 'airport_name', 'elevation_ft', 'iso_country', 'iso_region', 'municipality', 'gps_code', 'local_code', 'coordinates'],
-        'Description': [
-            'Airport identifier',
-            'Airport type',
-            'Airport name',
-            'Elevation in feet',
-            'ISO country code',
-            'ISO region code',
-            'Municipality',
-            'GPS code',
-            'Local code',
-            'Coordinates'
+        'Columns': [
+            ('ident', 'Airport identifier', 'String', 'JFK'),
+            ('type', 'Airport type', 'String', 'Large'),
+            ('airport_name', 'Airport name', 'String', 'John F. Kennedy International Airport'),
+            ('elevation_ft', 'Elevation in feet', 'Integer', '10'),
+            ('iso_country', 'ISO country code', 'String', 'US'),
+            ('iso_region', 'ISO region code', 'String', 'US-NY'),
+            ('municipality', 'Municipality', 'String', 'New York'),
+            ('gps_code', 'GPS code', 'String', 'JFK'),
+            ('local_code', 'Local code', 'String', 'JFK'),
+            ('coordinates', 'Coordinates', 'String', '40.6413° N, 73.7781° W')
         ]
     }
 }
 
-# Create a beautiful text file
-with open('data_dictionary/data_dictionary.txt', 'w') as file:
-    file.write("/////////////////////\n")
-    file.write("// Data Dictionary //\n")
-    file.write("/////////////////////\n\n")
+# Create a beautiful Markdown file
+with open('data_dictionary/data_dictionary.md', 'w') as file:
+    file.write("# Data Dictionary\n\n")
 
     for table, columns in table_columns.items():
-        file.write("-------\n")
-        file.write(f"{table}\n")
-        file.write("-------\n")
-        for old, new, desc in zip(columns['Old Column Names'], columns['New Column Names'], columns['Description']):
-            file.write(f"- {old} -> {new}: {desc}\n")
+        file.write(f"## {table}\n")
+        file.write("| Column | Description | Data Type | Example |\n")
+        file.write("|--------|-------------|-----------|---------|\n")
+        for col, desc, data_type, example in columns['Columns']:
+            file.write(f"| {col} | {desc} | {data_type} | {example} |\n")
         file.write("\n")
